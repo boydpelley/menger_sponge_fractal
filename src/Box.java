1c1db4f910389b81145340cab2afd25ae57d862b
boydpelley <boydjrpelley@gmail.com>
2024-03-02 16:22:18 -0400

    Start over on menger sponge fractal - too complicated initially.

--- src/Box.java
+++ src/Box.java
@@ -1,89 +1,62 @@
-import java.util.ArrayList;
-
 public class Box {
     Vector3D pos;
     float r;
 
-    private double angleX = 0.02;
-    private double angleY = 0.02;
+    private final double angleX = 0.02;
+    private final double angleY = 0.02;
+
+    private double[][] cubeVertices = {
+            {-1, -1, -1},
+            {-1, -1, 1},
+            {-1, 1, -1},
+            {-1, 1, 1},
+            {1, -1, -1},
+            {1, -1, 1},
+            {1, 1, -1},
+            {1, 1, 1}
+    };
+
+    private int[][] cubeEdges = {
+            {0, 1}, {1, 3}, {3, 2}, {2, 0},
+            {4, 5}, {5, 7}, {7, 6}, {6, 4},
+            {0, 4}, {1, 5}, {2, 6}, {3, 7}
+    };
 
     Box(float x, float y, float z, float r) {
         pos = new Vector3D(x, y, z);
         this.r = r;
     }
 
-    public ArrayList<Box> generate() {
-        ArrayList<Box> boxes = new ArrayList<>();
+    public double[][] getCubeVertices() {
+        return cubeVertices;
+    }
 
-        for (int x = -1; x < 2; x++) {
-            for (int y = -1; y < 2; y++) {
-                for (int z = -1; z < 2; z++) {
-                    float newR = r / 3;
-                    Box b = new Box(pos.getX() + x * newR, pos.getY() + y * newR, pos.getZ() + z * newR, newR);
-                    boxes.add(b);
-                }
-            }
-        }
-
-        return boxes;
+    public int[][] getCubeEdges() {
+        return cubeEdges;
     }
 
     public void rotateCube() {
-        // Rotate the box around X-axis
         double cosX = Math.cos(angleX);
         double sinX = Math.sin(angleX);
-        float rotatedY = (float) (pos.getY() * cosX + pos.getZ() * sinX);
-        float rotatedZ = (float) (-pos.getY() * sinX + pos.getZ() * cosX);
-
-        // Rotate the box around Y-axis
         double cosY = Math.cos(angleY);
         double sinY = Math.sin(angleY);
-        float rotatedX = (float) (pos.getX() * cosY + rotatedZ * sinY);
-        float rotatedZFinal = (float) (-pos.getX() * sinY + rotatedZ * cosY);
 
-        // Update the box position
-        pos.setX(rotatedX);
-        pos.setY(rotatedY);
-        pos.setZ(rotatedZFinal);
+        for (int i = 0; i < cubeVertices.length; i++) {
+            double x = cubeVertices[i][0];
+            double y = cubeVertices[i][1];
+            double z = cubeVertices[i][2];
+
+
+            cubeVertices[i][0] = x * cosY - z * sinY;
+            cubeVertices[i][2] = z * cosY + x * sinY;
+
+            z = cubeVertices[i][2];
+
+            cubeVertices[i][1] = y * cosX - z * sinX;
+            cubeVertices[i][2] = z * cosX + y * sinX;
+        }
     }
 
-    public float[] getVertices() {
-        float halfSize = r / 2;
-        float[] vertices = new float[24];
 
-        vertices[0] = pos.getX() - halfSize;
-        vertices[1] = pos.getY() - halfSize;
-        vertices[2] = pos.getZ() + halfSize;
 
-        vertices[3] = pos.getX() + halfSize;
-        vertices[4] = pos.getY() - halfSize;
-        vertices[5] = pos.getZ() + halfSize;
-
-        vertices[6] = pos.getX() + halfSize;
-        vertices[7] = pos.getY() + halfSize;
-        vertices[8] = pos.getZ() + halfSize;
-
-        vertices[9] = pos.getX() - halfSize;
-        vertices[10] = pos.getY() + halfSize;
-        vertices[11] = pos.getZ() + halfSize;
-
-        vertices[12] = pos.getX() - halfSize;
-        vertices[13] = pos.getY() - halfSize;
-        vertices[14] = pos.getZ() - halfSize;
-
-        vertices[15] = pos.getX() + halfSize;
-        vertices[16] = pos.getY() - halfSize;
-        vertices[17] = pos.getZ() - halfSize;
-
-        vertices[18] = pos.getX() + halfSize;
-        vertices[19] = pos.getY() + halfSize;
-        vertices[20] = pos.getZ() - halfSize;
-
-        vertices[21] = pos.getX() - halfSize;
-        vertices[22] = pos.getY() + halfSize;
-        vertices[23] = pos.getZ() - halfSize;
-
-        return vertices;
-    }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.Path2D;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
 
+    Box b = new Box(0, 0, 0, 200);
+
     public Main() {
-        FractalPanel fractalPanel = new FractalPanel();
-        JButton growButton = new JButton("Grow Fractal");
-        growButton.addActionListener(e -> {
-            fractalPanel.growFractal();
-            fractalPanel.repaint();
-        });
-
-        setLayout(new BorderLayout());
-        add(fractalPanel, BorderLayout.CENTER);
-        add(growButton, BorderLayout.SOUTH);
-
-        setSize(800, 600);
+        setTitle("Rotating cube");
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
 
-        Timer timer = new Timer(16, e -> {
-            fractalPanel.rotateFractal();
-            fractalPanel.repaint();
+        Timer timer = new Timer(16, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                b.rotateCube();
+                repaint();
+            }
         });
         timer.start();
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+
+        double [][] cubeVertices = b.getCubeVertices();
+        int [][] cubeEdges = b.getCubeEdges();
+
+        for (int[] edge : cubeEdges) {
+            int x1 = (int) (centerX + cubeVertices[edge[0]][0] * 50);
+            int y1 = (int) (centerY - cubeVertices[edge[0]][1] * 50);
+            int x2 = (int) (centerX + cubeVertices[edge[1]][0] * 50);
+            int y2 = (int) (centerY - cubeVertices[edge[1]][1] * 50);
+
+            g2d.drawLine(x1, y1, x2, y2);
+        }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Main());
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
     }
 }
